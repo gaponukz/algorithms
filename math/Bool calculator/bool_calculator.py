8585075b3288bba6d97b38da7ab5dbff316871ce
gaponukz <gaponukz54@gmail.com>
2023-01-23 01:19:57 +0200

    Update

--- math/Bool calculator/bool_calculator.py
+++ math/Bool calculator/bool_calculator.py
@@ -1,92 +1,16 @@
-from stack import Stack
-import nltk
-import unittest
+import string
+import itertools
 
-OPERATORS = { "or": 1, "and": 2, "not": 3 }
+from expression import Expression
 
-class Expression(object):
-    def __init__(self, str_expression: str):
-        self.str_expression = str_expression
-        self.converted_expression = self.convert()
-    
-    def convert(self):
-        infix_list: list[str] = nltk.tokenize.word_tokenize(self.str_expression)
-        postfix_list = []
-        stack = Stack()
+def print_bool_table(str_expression: str) -> None:
+    variables = list(set([item[1] for item in string.Formatter().parse(str_expression) if item[1] is not None]))
 
-        for token in infix_list:
-            if token in OPERATORS:
-                while not stack.empty():
-                    prev = stack.top()
+    for bool_args in list(itertools.product(["0", "1"], repeat=len(variables))):
+        formtaed_expression = str_expression.format(**{ variables[i]: bool_args[i] for i in range(len(variables)) })
+        result = Expression(formtaed_expression).apply()
 
-                    if prev in OPERATORS and OPERATORS[prev] >= OPERATORS[token]:
-                        stack.pop()
-                        postfix_list.append(prev) 
-                    
-                    else:
-                        break
-                
-                stack.push(token)
-            
-            elif token == "(":
-                stack.push(token)
-            
-            elif token == ")":
-                it = stack.pop()
-
-                while it != "(":
-                    postfix_list.append(it)
-                    it = stack.pop()
-            
-            else:
-                postfix_list.append(token)
-
-        while not stack.empty():
-            postfix_list.append(stack.pop())
-
-        return postfix_list
-
-    @staticmethod
-    def operation(left: bool, right: bool, operator: str) -> bool:
-        assert operator in OPERATORS
-
-        left, right = bool(int(left)), bool(int(right))
-
-        if operator == "or":
-            return left or right
-        
-        if operator == "and":
-            return left and right
-        
-        if operator == "not":
-            return not right
-
-    def apply(self):
-        stack = Stack()
-
-        for token in self.converted_expression:
-            if token in OPERATORS:
-                if token == "not":
-                    right_operand = stack.pop()
-                    
-                    res = self.operation(0, right_operand, token)
-                
-                else:
-                    right_operand = stack.pop()
-                    left__operand = stack.pop()
-                    
-                    res = self.operation(left__operand, right_operand, token)
-
-                stack.push(res)
-            
-            else:
-                stack.push(token)
-            
-        return stack.pop()
-
-class TestBoolCalculator(unittest.TestCase):
-    def test_all_what_can(self):
-        self.assertEqual(Expression("1 or (1 and not 0)").apply(), 1)
+        print(formtaed_expression + " = " + str(int(result)))
 
 if __name__ == "__main__":
-    unittest.main()
+    print_bool_table("{x} and {y} or not ({z} and {y})")
---
+++ math/Bool calculator/expression.py
@@ -0,0 +1,89 @@
+import nltk
+from stack import Stack
+
+OPERATORS = {
+    "or": {
+        "priority": 1,
+        "function": lambda a, b: a or b
+    },
+        "and": {
+        "priority": 2,
+        "function": lambda a, b: a and b
+    },
+        "not": {
+        "priority": 3,
+        "function": lambda _, b: not b
+    }
+}
+
+class Expression(object):
+    def __init__(self, str_expression: str):
+        self.str_expression = str_expression
+        self.converted_expression = self.convert()
+    
+    def convert(self):
+        infix_list: list[str] = nltk.tokenize.word_tokenize(self.str_expression)
+        postfix_list = []
+        stack = Stack()
+
+        for token in infix_list:
+            if token in OPERATORS:
+                while not stack.empty():
+                    prev = stack.top()
+
+                    if prev in OPERATORS and OPERATORS[prev]['priority'] >= OPERATORS[token]['priority']:
+                        stack.pop()
+                        postfix_list.append(prev) 
+                    
+                    else:
+                        break
+                
+                stack.push(token)
+            
+            elif token == "(":
+                stack.push(token)
+            
+            elif token == ")":
+                it = stack.pop()
+
+                while it != "(":
+                    postfix_list.append(it)
+                    it = stack.pop()
+            
+            else:
+                postfix_list.append(token)
+
+        while not stack.empty():
+            postfix_list.append(stack.pop())
+
+        return postfix_list
+    
+    def operation(self, left: bool, right: bool, operator: str) -> bool:
+        assert operator in OPERATORS
+
+        left, right = bool(int(left)), bool(int(right))
+        
+        return OPERATORS[operator]['function'](left, right)
+
+    def apply(self):
+        stack = Stack()
+
+        for token in self.converted_expression:
+            if token in OPERATORS:
+                if token == "not":
+                    right_operand = stack.pop()
+                    
+                    res = self.operation(0, right_operand, token)
+                
+                else:
+                    right_operand = stack.pop()
+                    left__operand = stack.pop()
+                    
+                    res = self.operation(left__operand, right_operand, token)
+
+                stack.push(res)
+            
+            else:
+                stack.push(token)
+            
+        return stack.pop()
