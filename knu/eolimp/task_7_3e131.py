1bf3c9f708ab65cdc8b0e899dd4418907dd3ed53
gaponukz <gaponukz54@gmail.com>
2023-03-16 19:00:26 +0200

    Some stupid things

--- knu/eolimp/task_7_3e131.py
+++ knu/eolimp/task_7_3e131.py
@@ -1,19 +1,90 @@
-# https://www.eolymp.com/uk/submissions/13226697
+# https://www.eolymp.com/uk/submissions/13250383
+import dataclasses
+import typing
 
-from collections import defaultdict
+@dataclasses.dataclass
+class HashTableItem:
+    key: typing.Hashable
+    value: typing.Any
+
+class HashTable:
+    __slots__ = 'size', 'storage'
+
+    def __init__(self, **kwargs: typing.Any):
+        self.size: typing.Final = 10001
+        self.storage: list[list[HashTableItem]] = [[] for _ in range(self.size)]
+
+        for key in kwargs:
+            self[key] = kwargs[key]
+
+    def get(self, key: typing.Hashable, default: typing.Any = None) -> typing.Any:
+        try:
+            return self[key]
+        except KeyError:
+            return default
+
+    def __getitem__(self, key: typing.Hashable) -> typing.Any:
+        key_hash = hash(key) % self.size
+
+        for item in self.storage[key_hash]:
+            if item.key == key:
+                return item.value
+
+        raise KeyError(key)
+
+    def __setitem__(self, key: typing.Hashable, value: typing.Any) -> None:
+        key_hash = hash(key) % self.size
+
+        if not self.storage[key_hash]:
+            self.storage[key_hash].append(HashTableItem(key, value))
+        else:
+            for item in self.storage[key_hash]:
+                if item.key == key:
+                    item.value = value
+                    return
+        
+            self.storage[key_hash].append(HashTableItem(key, value))
+
+    def __contains__(self, key: typing.Hashable) -> bool:
+        try:
+            self[key]
+            return True
+        except KeyError:
+            return False
+
+    def __delitem__(self, key: typing.Hashable) -> None:
+        key_hash = hash(key) % self.size
+
+        for item in self.storage[key_hash]:
+            if item.key == key:
+                self.storage[key_hash].remove(item)
+                return
+        
+        raise KeyError(key)
+
 
 s = input()
 n = int(input())
-mp = defaultdict(int)
+mp = HashTable()
+
 for ch in s:
+    if not mp.get(ch):
+        mp[ch] = 0
+    
     mp[ch] += 1
 
 ans = 0
 for i in range(n):
     cur = input()
     success = True
-    mcur = defaultdict(int)
+    mcur = HashTable()
     for ch in cur:
+        if not mcur.get(ch):
+            mcur[ch] = 0
+        
+        if not mp.get(ch):
+            mp[ch] = 0
+        
         mcur[ch] += 1
         if mcur[ch] > mp[ch]:
             success = False
--- knu/eolimp/task_7_4_e1236.py
+++ knu/eolimp/task_7_4_e1236.py
@@ -1,9 +1,69 @@
-# https://www.eolymp.com/uk/submissions/13239101
+# https://www.eolymp.com/uk/submissions/13250300
 
+import dataclasses
 import typing
-from collections import defaultdict
 
 T = typing.TypeVar("T", bound=int)
+
+@dataclasses.dataclass
+class HashTableItem:
+    key: typing.Hashable
+    value: typing.Any
+
+class HashTable:
+    __slots__ = 'size', 'storage'
+
+    def __init__(self, **kwargs: typing.Any):
+        self.size: typing.Final = 10001
+        self.storage: list[list[HashTableItem]] = [[] for _ in range(self.size)]
+
+        for key in kwargs:
+            self[key] = kwargs[key]
+
+    def get(self, key: typing.Hashable, default: typing.Any = None) -> typing.Any:
+        try:
+            return self[key]
+        except KeyError:
+            return default
+
+    def __getitem__(self, key: typing.Hashable) -> typing.Any:
+        key_hash = hash(key) % self.size
+
+        for item in self.storage[key_hash]:
+            if item.key == key:
+                return item.value
+
+        raise KeyError(key)
+
+    def __setitem__(self, key: typing.Hashable, value: typing.Any) -> None:
+        key_hash = hash(key) % self.size
+
+        if not self.storage[key_hash]:
+            self.storage[key_hash].append(HashTableItem(key, value))
+        else:
+            for item in self.storage[key_hash]:
+                if item.key == key:
+                    item.value = value
+                    return
+        
+            self.storage[key_hash].append(HashTableItem(key, value))
+
+    def __contains__(self, key: typing.Hashable) -> bool:
+        try:
+            self[key]
+            return True
+        except KeyError:
+            return False
+
+    def __delitem__(self, key: typing.Hashable) -> None:
+        key_hash = hash(key) % self.size
+
+        for item in self.storage[key_hash]:
+            if item.key == key:
+                self.storage[key_hash].remove(item)
+                return
+        
+        raise KeyError(key)
 
 def binary_search(data: list[T], x: T) -> T:
     low, high = 0, len(data) - 1
@@ -21,10 +81,13 @@ def binary_search(data: list[T], x: T) -> T:
 
     return result
 
-def get_letters_info(string: str) -> defaultdict[str, list[int]]:
-    result = defaultdict(list)
+def get_letters_info(string: str) -> HashTable:
+    result = HashTable()
 
     for index, char in enumerate(string):
+        if not result.get(char):
+            result[char] = []
+        
         result[char].append(index)
 
     return result
@@ -42,7 +105,7 @@ def main():
 
         for char in word:
             last = indexes[-1] if indexes else -1
-            item = binary_search(text_letters_info[char], last)
+            item = binary_search(text_letters_info.get(char, []), last)
 
             if item != -1:
                 indexes.append(item)
